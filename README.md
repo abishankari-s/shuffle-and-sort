ce0a2b1a7e3903abf96e54f340ed0f6794c2c781
abishankari-s <abishankari.s@maersk.com>
2023-07-21 17:20:15 +0530

    Initial commit of application

---
+++ README.md
@@ -0,0 +1,16 @@
+# Shuffle and Sort🎲
+
+## Introduction
+This is the aplication of Shuffle and sort of 3x3 grids.
+
+## Languages Used
+* HTML
+* SCSS
+* Type Script
+
+## Project Setup
+Open the terminal to the project folder 
+1. Insatll packages -> npm install or npm i 
+2. Compile SCSS -> npx sass src/styles/index.scss dist/styles/index.css
+3. Compile Type Script -> npx tsc
+
---
+++ src/scripts/index.ts
@@ -0,0 +1,23 @@
+export function shuffleGridCells(): void {
+    const gridCells = document.querySelectorAll('.grid-cell');
+    const gridContainer = document.querySelector('.grid-container') as HTMLElement;
+    const cellsArray = Array.from(gridCells);
+    cellsArray.sort(() => Math.random() - 0.5);
+    cellsArray.forEach(cell => gridContainer.appendChild(cell));
+  }
+  
+export function sortGridCells(): void {
+    const gridContainer = document.querySelector('.grid-container') as HTMLElement;
+    const gridCells = Array.from(gridContainer.children) as HTMLElement[];
+    gridCells.sort((a, b) => {
+      return parseInt(a.textContent || '0') - parseInt(b.textContent || '0');
+    });
+    gridCells.forEach(cell => gridContainer.appendChild(cell));
+  }
+
+  const shuffleBtn = document.getElementById('shuffleBtn');
+  shuffleBtn?.addEventListener('click', shuffleGridCells);
+ 
+  const sortBtn = document.getElementById('sortBtn');
+  sortBtn?.addEventListener('click', sortGridCells);
+  
